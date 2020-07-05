c86387f81a27d978bea102e7c04b9367e4cb733f
Timothy <geroginalam@hotmail.com>
2020-07-06 03:01:13 +0800

    refactor

--- codes/blinkDetect.py
+++ codes/blinkDetect.py
@@ -10,45 +10,29 @@ TODO:
 """
 
 import dlib
-import sys
 import cv2
 import time
 import numpy as np
-from scipy.spatial import distance as dist
 from threading import Thread
 import playsound
 import queue
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import imutils
-# from light_variability import adjust_gamma
+from logic import *
 
-FACE_DOWNSAMPLE_RATIO = 1.5
 RESIZE_HEIGHT = 460
-
-thresh = 0.3
-modelPath = "models/shape_predictor_70_face_landmarks.dat"
-cascade = "models/haarcascade_frontalface_default.xml"
-sound_path = "alarm.wav"
-
-# detector = dlib.get_frontal_face_detector()
-detector = cv2.CascadeClassifier(cascade)
-predictor = dlib.shape_predictor(modelPath)
-
-leftEyeIndex = [36, 37, 38, 39, 40, 41]
-rightEyeIndex = [42, 43, 44, 45, 46, 47]
-
 blinkCount = 0
 drowsy = 0
 state = 0
 blinkTime = 0.15 #150ms
 drowsyTime = 1.0  #1200ms
 ALARM_ON = False
-GAMMA = 1.5
-threadStatusQ = queue.Queue()
+leftEyeIndex = [36, 37, 38, 39, 40, 41]
+rightEyeIndex = [42, 43, 44, 45, 46, 47]
 
-invGamma = 1.0/GAMMA
-table = np.array([((i / 255.0) ** invGamma) * 255 for i in range(0, 256)]).astype("uint8")
+sound_path = "../sound/alarm.wav"
+
+threadStatusQ = queue.Queue()
 
 class PiVideoStream:
     def __init__(self, resolution=(640,480), framerate=30):
@@ -82,13 +66,6 @@ class PiVideoStream:
     
     def stop(self):
         self.stopped = True
-        
-def gamma_correction(image):
-    return cv2.LUT(image, table)
-
-def histogram_equalization(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return cv2.equalizeHist(gray) 
 
 def soundAlert(path, threadStatusQ):
     while True:
@@ -97,101 +74,6 @@ def soundAlert(path, threadStatusQ):
             if FINISHED:
                 break
         playsound.playsound(path)
-
-def eye_aspect_ratio(eye):
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
-    C = dist.euclidean(eye[0], eye[3])
-    ear = (A + B) / (2.0 * C)
-
-    return ear
-
-
-def checkEyeStatus(landmarks):
-    mask = np.zeros(frame.shape[:2], dtype = np.float32)
-    
-    hullLeftEye = []
-    for i in range(0, len(leftEyeIndex)):
-        hullLeftEye.append((landmarks[leftEyeIndex[i]][0], landmarks[leftEyeIndex[i]][1]))
-
-    cv2.fillConvexPoly(mask, np.int32(hullLeftEye), 255)
-
-    hullRightEye = []
-    for i in range(0, len(rightEyeIndex)):
-        hullRightEye.append((landmarks[rightEyeIndex[i]][0], landmarks[rightEyeIndex[i]][1]))
-
-
-    cv2.fillConvexPoly(mask, np.int32(hullRightEye), 255)
-
-    # lenLeftEyeX = landmarks[leftEyeIndex[3]][0] - landmarks[leftEyeIndex[0]][0]
-    # lenLeftEyeY = landmarks[leftEyeIndex[3]][1] - landmarks[leftEyeIndex[0]][1]
-
-    # lenLeftEyeSquared = (lenLeftEyeX ** 2) + (lenLeftEyeY ** 2)
-    # eyeRegionCount = cv2.countNonZero(mask)
-
-    # normalizedCount = eyeRegionCount/np.float32(lenLeftEyeSquared)
-
-    #############################################################################
-    leftEAR = eye_aspect_ratio(hullLeftEye)
-    rightEAR = eye_aspect_ratio(hullRightEye)
-
-    ear = (leftEAR + rightEAR) / 2.0
-    #############################################################################
-
-    eyeStatus = 1          # 1 -> Open, 0 -> closed
-    if (ear < thresh):
-        eyeStatus = 0
-
-    return eyeStatus  
-
-def checkBlinkStatus(eyeStatus):
-    global state, blinkCount, drowsy
-    if(state >= 0 and state <= falseBlinkLimit):
-        if(eyeStatus):
-            state = 0
-
-        else:
-            state += 1
-
-    elif(state >= falseBlinkLimit and state < drowsyLimit):
-        if(eyeStatus):
-            blinkCount += 1 
-            state = 0
-
-        else:
-            state += 1
-
-
-    else:
-        if(eyeStatus):
-            state = 0
-            drowsy = 1
-            blinkCount += 1
-
-        else:
-            drowsy = 1
-
-def getLandmarks(im):
-    imSmall = cv2.resize(im, None, 
-                            fx = 1.0/FACE_DOWNSAMPLE_RATIO, 
-                            fy = 1.0/FACE_DOWNSAMPLE_RATIO, 
-                            interpolation = cv2.INTER_LINEAR)
-
-    #rects = detector(imSmall, 0)
-    rects = detector.detectMultiScale(imSmall, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-    if len(rects) == 0:
-        return 0
-
-    arects = rects[0];
-    newRect = dlib.rectangle(int(rects[0][0] * FACE_DOWNSAMPLE_RATIO),
-                             int(rects[0][1] * FACE_DOWNSAMPLE_RATIO),
-                             int((rects[0][2] + rects[0][0]) * FACE_DOWNSAMPLE_RATIO),
-                             int((rects[0][3] + rects[0][1]) * FACE_DOWNSAMPLE_RATIO)
-                            )
-
-    points = []
-    [points.append((p.x, p.y)) for p in predictor(im, newRect).parts()]
-    return points
 
 if __name__ == "__main__":
 
@@ -250,7 +132,7 @@ if __name__ == "__main__":
     print("height: " + str(height))
 
     #vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*"DIVX"), 15, (frame.shape[1] * 2, frame.shape[0] * 2))
-    vid_writer = cv2.VideoWriter('outputNew.avi',cv2.VideoWriter_fourcc(*"DIVX"), 15, (640, 480))
+    vid_writer = cv2.VideoWriter('outputNew.avi',cv2.VideoWriter_fourcc(*"DIVX"), 25, (640, 480))
     
     while(1):
         try:
